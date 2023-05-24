bb805cd6f249a916da001b7250ed3b0d511d6658
Jacky Andrazat <82565381+Jackyandrazat@users.noreply.github.com>
2023-05-24 17:46:12 +0700

    Update : Added GET and POST My pokemon Models
    Menambahkan Query untuk GET dan POST(add) My Pokemons

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -31,10 +31,26 @@ const deletePokemon = async (id) => {
     return dbPool.execute(sqlQuery)
 }
 
+// Section for Collection My Pokemon
+
+const getAllMyPokemon = async () => {
+    const sqlQuery = 'SELECT * FROM my_pokemons'
+    return dbPool.execute(sqlQuery);
+}
+
+const addToMyPokemon = async (id_pokemons, id_user) =>{
+    const [result] = await db.execute('INSERT INTO my_pokemons (id_pokemons, id_user) VALUES (?, ?)', [type, level]);
+    const id = result.insertId;
+    return { id, id_pokemons, id_user };
+}
+
 module.exports = {
     getAllPokemon,
     getDetailPokemon,
     createNewPokemon,
     updatePokemon,
-    deletePokemon
+    deletePokemon,
+    //MyPokemon
+    getAllMyPokemon,
+    addToMyPokemon,
 }
