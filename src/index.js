9774cb93d22bb21024722abf36a06bf1ff45f695
Jacky Andrazat <82565381+Jackyandrazat@users.noreply.github.com>
2023-05-29 11:25:20 +0700

    update index
    added acces

--- src/index.js
+++ src/index.js
@@ -22,6 +22,11 @@ const app = express()
 //   res.send("Hello World");
 // })
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 //middleware
 app.use(MiddlewarelogRequest);
 app.use(express.json());
