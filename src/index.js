69ebfafd3788fa2a7f6bc6afcc74dc7c5197114b
Jacky Andrazat <82565381+Jackyandrazat@users.noreply.github.com>
2023-05-31 11:07:47 +0700

    Update index.js
    add package cors to fixing  error acces

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const PORT = process.env.PORT || 5000;
 
 const express = require('express')
+const cors = require('cors');
 
 const authRoutes = require('./routes/auth')
 const pokemonsRoutes = require('./routes/pokemon')
@@ -21,6 +22,7 @@ const app = express()
 // app.use("/", (req, res, next) => {
 //   res.send("Hello World");
 // })
+app.use(cors());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
