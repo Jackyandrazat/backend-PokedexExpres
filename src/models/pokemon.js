51b121a777381c9948c641a1d2d6f4145e895a31
Jacky Andrazat <82565381+Jackyandrazat@users.noreply.github.com>
2023-06-06 22:48:01 +0700

    Update pokemon.js

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -39,6 +39,18 @@ const getAllMyPokemon = async () => {
     return dbPool.execute(sqlQuery);
 }
 
+const getMyPokemonById = async (id_user) => {
+    console.log(id_user, 'idddddd')
+    const sqlQuery = `SELECT my_pokemons.id, users.id AS id_user, pokemons.id AS pokemons_id, pokemons.name_pokemon, pokemons.type_pokemon, 
+    pokemons.ability_pokemon, pokemons.desckripsi_pokemon, pokemons.avatar_url 
+    FROM my_pokemons 
+    JOIN pokemons ON my_pokemons.id_pokemons = pokemons.id JOIN users ON my_pokemons.id_user = users.id
+    WHERE my_pokemons.id_user = ?`; // Menambahkan klausa WHERE dengan menggunakan placeholder ?
+  
+    return dbPool.execute(sqlQuery, [id_user]); // Menggunakan parameter id pada array sebagai nilai placeholder
+  };
+  
+
 const addToMyPokemon = async (id_pokemons, id_user) =>{
     const [result] = await dbPool.execute('INSERT INTO my_pokemons (id_pokemons, id_user) VALUES (?, ?)', [id_pokemons, id_user]);
     const id = result.insertId;
@@ -65,5 +77,6 @@ module.exports = {
     getAllMyPokemon,
     addToMyPokemon,
     deleteMyPokemon,
-    findAddMypokemon
+    findAddMypokemon,
+    getMyPokemonById
 }
